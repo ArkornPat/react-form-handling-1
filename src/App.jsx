53af9529b1b31d8d5fb0bebf1b4712dd50e97895
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-23 15:38:11 +0700

    feat: add react form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("New Greeting Message");
+  const [input, setInput] = useState("");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={input} onChange={(event)=>{setInput(event.target.value)}}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={()=>{setMessage(input)}}>Update text</button>
       </div>
     </div>
   );
